c4f0e5d5716adc751f6b6de2e3e96816f9207b7e
HarryQu1229 <Harryqu666@gmail.com>
2022-06-25 17:04:01 +1200

    implemented SearchBar component

--- src/pages/landingPage/LandingPage.js
+++ src/pages/landingPage/LandingPage.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import Recommended from '../../components/Recommended/Recommended';
-import TopPicks from '../../components/TopPicks/TopPicks';
+import React from "react";
+import Recommended from "../../components/Recommended/Recommended";
+import TopPicks from "../../components/TopPicks/TopPicks";
+import SearchBar from "../../components/General/SearchBar";
 
 const LandingPage = () => {
   return (
     <>
-    <TopPicks />
-    <Recommended />
+      <SearchBar isWide={false} />
+      <TopPicks />
+      <Recommended />
     </>
-  )
-}
+  );
+};
 
 export default LandingPage;
